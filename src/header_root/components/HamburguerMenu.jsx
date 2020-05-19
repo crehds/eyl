a3387f7c484cd7382c5f0aaae78a9b1f92e90317
Unknown <carlos15erh@gmail.com>
2020-05-18 20:49:27 -0500

    Primeros pasos creando el hamburguer

---
+++ src/header_root/components/HamburguerMenu.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import '../css/hamburguerMenu.css';
+
+export default class Hamburguer extends Component {
+  render() {
+    return <i className="icon-menu hamburguer-menu">
+
+    </i>;
+  }
+}
--- src/header_root/header.jsx
+++ src/header_root/header.jsx
@@ -2,15 +2,17 @@ import React, { Component } from "react";
 import './css/header.css'
 import Menu from './menu';
 import Logo from './logo';
+import HamburguerMenu from './components/HamburguerMenu';
 
 export default class Header extends Component {
   
   render() {
     return <header className="App-header">
       <Logo/>
-      <Menu
+      <HamburguerMenu/>
+      {/*<Menu
         handleContent={this.props.handleContent}
-      />
+      />*/}
     </header>;
   }
 }
