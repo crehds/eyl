d69e3c1c04ab3e568638c5802755bd2574702a02
Unknown <carlos15erh@gmail.com>
2020-05-21 19:56:41 -0500

    First Implementation of dance gender carousel
    Modifiqué la pantalla de inicio para mostrar los géneros de forma dinamica

--- src/main_root/components/Inicio.jsx
+++ src/main_root/components/Inicio.jsx
@@ -1,11 +1,44 @@
-import React, { Component } from 'react'
-import '../css/inicio.css'
-
+import React, { Component } from "react";
+import "../css/inicio.css";
+import Template from "./template";
 export default class Inico extends Component {
+  state = {
+    generos: [
+      "Salsa",
+      "Bachata",
+      "Jazz",
+      "Latin Urban",
+      "Ladies",
+      "Ballet",
+      "Urban Style",
+      "Mambo",
+      "Body Movement",
+    ],
+    bailes: [],
+  };
+
+  handleCarrusel = (generos) => {};
+
+  componentDidMount() {
+    this.state.generos.forEach((e, i) => {
+      setTimeout(
+        () =>
+          this.setState({
+            bailes: this.state.bailes.concat([{ id: i + 1, string: e }]),
+          }),
+        1000 * (i + 1)
+      );
+    });
+  }
   render() {
+    const { bailes } = this.state;
+
     return (
-      <div className="main1">       
+      <div className="main1">
+        {bailes.map((e) => (
+          <Template content={e} />
+        ))}
       </div>
-    )
+    );
   }
 }
