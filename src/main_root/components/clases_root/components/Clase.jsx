9d0c828543040bc0e2deb00d0c5e7ba1f038e2f9
Unknown <carlos15erh@gmail.com>
2020-06-07 16:55:13 -0500

    Integration of videos(assets and component)

--- src/main_root/components/clases_root/components/Clase.jsx
+++ src/main_root/components/clases_root/components/Clase.jsx
@@ -3,6 +3,6 @@ import "../css/clase.css";
 
 export default class Clase extends PureComponent {
   render() {
-    return <div className="clase">{this.props.content.string}</div>;
+    return <div className="clase" onClick={this.props.onclick}>{this.props.content.string}</div>;
   }
 }
---
+++ src/main_root/components/clases_root/components/Videos.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import "../css/videos.css";
+
+export default class Videos extends Component {
+  render() {
+    return (
+      <div className="videos">
+        <div className="description-video">
+          <i className="icon-arrow-left3 arrow-video" onClick={this.props.toggleContent}></i>
+          <h3>Género</h3>
+        </div>
+        <div className="videos-container">
+          <div className="video-box">
+            <video
+              src={process.env.PUBLIC_URL + "/videos/video_corto.mp4"}
+              controls
+            ></video>
+          </div>
+          <div className="video-box">
+            <video
+              src={process.env.PUBLIC_URL + "/videos/video_largo.mp4"}
+              controls
+            ></video>
+          </div>
+          <div className="video-box">
+            <video
+              src={process.env.PUBLIC_URL + "/videos/ladies_latinas.mp4"}
+              controls
+            ></video>
+          </div>
+          <div className="video-box">
+            <video
+              src={process.env.PUBLIC_URL + "/videos/como_llegar.mp4"}
+              controls
+            ></video>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
