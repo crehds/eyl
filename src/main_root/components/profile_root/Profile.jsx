96925a7de1f9cdc900934a37d2dd46994e3238e9
Unknown <carlos15erh@gmail.com>
2020-07-11 00:09:39 -0500

    first implementation of profile(after login)
    funcionamiento muy bajo aún

---
+++ src/main_root/components/profile_root/Profile.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./css/profile.css";
+import ProfileHead from "./components/ProfileHead";
+import ProfileBody from "./components/ProfileBody";
+// import ProfileFooter from "./components/ProfileFooter";
+
+export default class Profile extends Component {
+  render() {
+    return (
+      <div className="profile">
+        <ProfileHead />
+        <ProfileBody />
+      </div>
+    );
+  }
+}
